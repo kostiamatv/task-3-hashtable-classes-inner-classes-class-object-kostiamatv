8a8006b2cf9c27f72dd3928202f38ce94c914ebb
(◕‿◕) <kostiamatv@gmail.com>
2020-03-13 19:50:23 +0300

    success build

--- src/main/java/ru/itmo/java/HashTable.java
+++ src/main/java/ru/itmo/java/HashTable.java
@@ -1,23 +1,169 @@
 package ru.itmo.java;
 
-import java.util.Map;
-
 public class HashTable {
 
-    Object put(Object key, Object value) {
-        throw new UnsupportedOperationException();
+    private static final int DEFAULT_SIZE = 1000;
+    private static final double DEFAULT_LOAD_FACTOR = 0.5;
+
+    private int size = 0;
+    private Entity[] array;
+    private double loadFactor;
+
+    public HashTable() {
+        array = new Entity[DEFAULT_SIZE];
+        loadFactor = DEFAULT_LOAD_FACTOR;
     }
 
-    Object get(Object key) {
-        throw new UnsupportedOperationException();
+    public HashTable(int size) {
+        array = new Entity[size];
+        loadFactor = DEFAULT_LOAD_FACTOR;
     }
 
-    Object remove(Object key) {
-        throw new UnsupportedOperationException();
+    public HashTable(double loadFactor) {
+        array = new Entity[DEFAULT_SIZE];
+        this.loadFactor = loadFactor;
     }
 
-    int size() {
-        throw new UnsupportedOperationException();
+    public HashTable(int size, double loadFactor) {
+        array = new Entity[size];
+        this.loadFactor = loadFactor;
     }
 
+    public Object put(Object key, Object value) {
+        int index = getIndex(key, array.length);
+        Entity oldEntity = array[index];
+        array[index] = new Entity(key, value);
+        if (oldEntity == null || oldEntity.isDeleted()) {
+            size++;
+            checkSize();
+            return null;
+        } else {
+            return oldEntity.getValue();
+        }
+    }
+
+    public Object get(Object key) {
+        int index = getIndex(key, array.length);
+        if (array[index] == null || array[index].isDeleted()) {
+            return null;
+        } else {
+            return array[index].getValue();
+        }
+//        int index = Math.abs(key.hashCode()) % array.length;
+//        while (array[index] != null && array[index].notDeleted()) {
+//            if (array[index].getKey() == key && array[index].notDeleted()) {
+//                return array[index].getValue();
+//            }
+//            if(array[index].getKey() == key && array[index].isDeleted()){
+//                break;
+//            }
+//            index++;
+//            if (index == array.length) {
+//                index = 0;
+//            }
+//        }
+    }
+
+    public Object remove(Object key) {
+        int index = getIndex(key, array.length);
+        if (array[index] == null || array[index].isDeleted()) {
+            return null;
+        }
+        size--;
+        array[index].setDeleted();
+        return array[index].getValue();
+//        while (array[index] != null) {
+//            if (array[index].getKey() == key && array[index].notDeleted()) {
+//                array[index].setDeleted();
+//                size--;
+//                return array[index].getValue();
+//            }
+//            index++;
+//            if (index == array.length) {
+//                index = 0;
+//            }
+//        }
+//        return null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private int getIndex(Object key, int arrayLength) {
+        int hash = Math.abs(key.hashCode());
+        int index = hash % arrayLength;
+        while (array[index] != null && !array[index].getKey().equals(key)) {
+            index++;
+            if (index == array.length) {
+                index = 0;
+            }
+            if (index == hash % arrayLength) {
+                break;
+            }
+        }
+        if (array[index] == null || array[index].getKey().equals(key)) {
+            return index;
+        }
+        index = hash % arrayLength;
+        while (array[index].notDeleted()) {
+            index++;
+            if (index == array.length) {
+                index = 0;
+            }
+        }
+        return index;
+    }
+
+    private int getThreshold() {
+        return (int) (loadFactor * array.length);
+    }
+
+    private void checkSize() {
+        if (size >= getThreshold()) {
+            increaseSize();
+        }
+    }
+
+    private void increaseSize() {
+        var oldArray = array;
+        array = new Entity[array.length * 2];
+        for (Entity entity : oldArray) {
+            if (entity != null && entity.notDeleted()) {
+                array[getIndex(entity.getKey(), array.length)] = entity;
+            }
+        }
+    }
+
+    private class Entity {
+        private Object key;
+        private Object value;
+        private boolean deleted;
+
+        public Entity(Object key, Object value) {
+            this.key = key;
+            this.value = value;
+            deleted = false;
+        }
+
+        public Object getKey() {
+            return key;
+        }
+
+        public Object getValue() {
+            return value;
+        }
+
+        public boolean notDeleted() {
+            return !deleted;
+        }
+
+        public boolean isDeleted() {
+            return deleted;
+        }
+
+        public void setDeleted() {
+            deleted = true;
+        }
+    }
 }
